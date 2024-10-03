be28e5cd7434ad57a057d4867876de016b39137f
coderotsch <168534330+coderotsch@users.noreply.github.com>
2024-10-03 16:55:41 +0200

    Update hello.js

--- functions/hello.js
+++ functions/hello.js
@@ -1,8 +1,11 @@
 // netlify/functions/hello.js
+
 exports.handler = async function(event, context) {
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: "Hello, World!" }),
-    };
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: "Hello, World!" }),
+    headers: {
+      "Content-Type": "application/json"
+    }
   };
-  
+};
